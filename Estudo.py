4b276b0e01836f8e5ee0e453aee4cc125eab4a28
Gustavo Barros Tenório <129299486+gustavotenorio26062005@users.noreply.github.com>
2024-07-06 13:15:36 -0300

    Add files via upload

---
+++ Estudo.py
@@ -0,0 +1,16 @@
+nome = input('Qual seu nome? \n')
+print('Olá, ' + nome)
+
+dia = input('Qual dia você nasceu? \n')
+mes = input('Qual mês você nasceu? \n')
+ano = input('Qual ano você nasceu? \n')
+
+print(nome+' Sua data de nascimento é: '+ dia + '/'+ mes + '/'+ ano + '/')
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+print('A soma deles é: ',num1+num2)
+
+digitar = input('Digite algo: ')
+tipo = type(digitar)
